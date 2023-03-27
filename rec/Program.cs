ba16e9fc612f2162494feded674a79c6549a4f32
aleks <aleks@DESKTOP-A6TLPDO>
2023-03-27 09:17:18 +0300

    Добавьте файлы проекта.

---
+++ rec/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
